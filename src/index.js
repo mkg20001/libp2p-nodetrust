35885bd269fd24d024e9184373a8fd9bd693493e
mkg20001 <mkg20001@gmail.com>
2018-02-27 17:02:23 +0100

    fix: #2

--- src/index.js
+++ src/index.js
@@ -6,21 +6,25 @@ const multiaddr = require('multiaddr')
 const {waterfall} = require('async')
 const Peer = require('peer-info')
 const Id = require('peer-id')
-const defaultNode = new Peer(Id.createFromB58String('Qm'))
 const once = require('once')
 const debug = require('debug')
 const log = debug('libp2p:nodetrust')
 const noop = err => err ? log(err) : false
-defaultNode.multiaddrs.add('/dnsaddr/libp2p-nodetrust.tk/tcp/8899')
+
+const defaultNode = new Peer(Id.createFromB58String('Qm'))
+defaultNode.multiaddrs.add('/dnsaddr/nodetrust.libp2p.io/tcp/8899')
 
 const nat = require('nat-puncher')
 const protocolMuxer = require('libp2p-switch/src/protocol-muxer')
+
+const Discovery = require('./discovery')
 
 module.exports = class Nodetrust {
   constructor (opt) {
     this.node = opt.node || defaultNode
   }
   __setSwarm (swarm) {
+    this.discovery = new Discovery(this.swarm)
     this.swarm = swarm
     this.ws = new WS()
   }
@@ -49,9 +53,9 @@ module.exports = class Nodetrust {
 
     log('starting wss server')
 
-    if (!this.swarm.switch.transports.WebSockets) this.swarm.switch.transports.WebSockets = this.ws
+    if (!this.swarm.switch.transports.WebSockets) this.swarm.switch.transports.WebSockets = this.ws // HACK: hack in the wss transport for dialing. (needs some way to add transports at runtime)
 
-    // HACK: hack in the wss server as a transport
+    // HACK: hack in the wss server as a transport (needs some way to change listeners at runtime)
     this.wss = this.ws.createListener({
       cert: this.cert.chain,
       key: this.cert.key
@@ -101,13 +105,13 @@ module.exports = class Nodetrust {
     waterfall([
       cb => this._getCert(cb),
       (cert, cb) => {
-        // this._renewTimeout = setTimeout(this._renew.bind(this), cert.expiresAt - Date.now() - 1000) // leave 1s gap to avoid ._getCert race
+        this._renewInterval = setInterval(this._renew.bind(this), 10000)
         this._setupServer(cb)
       },
-      (addr, cb) => {
+      (wss, cb) => {
         const {cert, swarm} = this
         const {peerInfo} = swarm
-        const ma = this._addedMAs = cert.altnames.map(domain => '/dnsaddr/' + domain + '/tcp/' + addr.port + '/wss/ipfs/' + swarm.peerInfo.id.toB58String())
+        const ma = this._addedMAs = cert.altnames.map(domain => '/dnsaddr/' + domain + '/tcp/' + wss.port + '/wss/ipfs/' + swarm.peerInfo.id.toB58String())
         ma.forEach(addr => peerInfo.multiaddrs.add(addr))
         cb()
       }
@@ -118,8 +122,8 @@ module.exports = class Nodetrust {
     waterfall([
       cb => this._shutdownServer(cb),
       cb => {
-        const {_addedMAs, swarm, _renewTimeout} = this
-        clearTimeout(_renewTimeout || 0)
+        const {_addedMAs, swarm, _renewInterval} = this
+        clearInterval(_renewInterval || 0)
         const {peerInfo} = swarm
         peerInfo.multiaddrs.replace(_addedMAs, [])
         cb()
@@ -128,6 +132,7 @@ module.exports = class Nodetrust {
   }
 
   _renew (cb) {
+    if (this.cert.expiresAt + 1000 > Date.now()) return
     log('renewing')
     waterfall([
       cb => this._shutdown(cb),
