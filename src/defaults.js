d27d9438010592e038f6eca1484bd5df1db6dfad
mkg20001 <mkg20001@gmail.com>
2018-02-28 19:14:39 +0100

    The same thing as last commit...

--- src/defaults.js
+++ src/defaults.js
@@ -8,7 +8,7 @@ let defaultNode
 defaultNode = new Peer(Id.createFromB58String('QmQvFUNc1pKcUAoekE1XxS5TsMSDB9dw5CYkRxRiDGfFsX'))
 if (process.toString() === '[object process]') { // node
   /* defaultNode.multiaddrs.add('/dnsaddr/nodetrust.libp2p.io/tcp/8899/ipfs/Qm') */
-  defaultNode.multiaddrs.add('/ip4/88.99.229.51/tcp/8877/ipfs/QmQvFUNc1pKcUAoekE1XxS5TsMSDB9dw5CYkRxRiDGfFsX')
+  defaultNode.multiaddrs.add('/ip4/88.99.229.51/tcp/8899/ipfs/QmQvFUNc1pKcUAoekE1XxS5TsMSDB9dw5CYkRxRiDGfFsX')
 } else { // browser
   /* defaultNode.multiaddrs.add('/dnsaddr/nodetrust.libp2p.io/tcp/443/wss/ipfs/Qm') */
   defaultNode.multiaddrs.add('/dnsaddr/libp2p-nodetrust.tk/tcp/443/wss/ipfs/QmQvFUNc1pKcUAoekE1XxS5TsMSDB9dw5CYkRxRiDGfFsX')
