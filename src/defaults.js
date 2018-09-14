2d5fbefe3b07c97e36d866e4c7ed294f3a02a083
Maciej Krüger <mkg20001@gmail.com>
2018-09-14 14:17:36 +0200

    feat: Use dns only

--- src/defaults.js
+++ src/defaults.js
@@ -9,8 +9,6 @@ defaultNode = new Peer(Id.createFromB58String('QmP44NJN9MyhZCTcPXqBNroW9ekkp5PFr
 if (process.toString() === '[object process]') { // node
   /* defaultNode.multiaddrs.add('/dnsaddr/nodetrust.libp2p.io/tcp/8899/ipfs/Qm') */
   defaultNode.multiaddrs.add('/dnsaddr/issue.libp2p-nodetrust.tk/tcp/25893/ipfs/QmP44NJN9MyhZCTcPXqBNroW9ekkp5PFrMQRxRhuSzd3Mz')
-  defaultNode.multiaddrs.add('/ip4/138.201.254.83/tcp/25893/ipfs/QmP44NJN9MyhZCTcPXqBNroW9ekkp5PFrMQRxRhuSzd3Mz')
-  defaultNode.multiaddrs.add('/ip6/2a01:4f8:173:2552::2/tcp/25893/ipfs/QmP44NJN9MyhZCTcPXqBNroW9ekkp5PFrMQRxRhuSzd3Mz')
 } else { // browser
   /* defaultNode.multiaddrs.add('/dnsaddr/nodetrust.libp2p.io/tcp/443/wss/ipfs/Qm') */
   defaultNode.multiaddrs.add('/dnsaddr/libp2p-nodetrust.tk/tcp/443/wss/ipfs/QmP44NJN9MyhZCTcPXqBNroW9ekkp5PFrMQRxRhuSzd3Mz')
